384be503d7188f33ab26afb8a861ee4775c54a90
Patrick Kubiak <patrick@antuple.net>
2016-10-27 15:04:18 -0400

    implemented service worker with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
+var swPrecache = require('sw-precache');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
+var path = require('path');
 
-gulp.task('default', ['vulcanize', 'replace', 'copy']);
+gulp.task('default', ['vulcanize', 'generate-service-worker', 'replace', 'copy']);
 
 gulp.task('vulcanize', function () {
     gulp.src('src/components.html')
@@ -16,6 +18,14 @@ gulp.task('vulcanize', function () {
       .pipe(gulp.dest('./build'));
 });
 
+gulp.task('generate-service-worker', function(callback) {
+  var rootDir = 'assets';
+
+  swPrecache.write(path.join('build', 'service-worker.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}']
+  }, callback);
+});
+
 gulp.task('replace', function() {
     gulp.src('index.html')
       .pipe(replace('src/components.html', 'critical.html'))
@@ -24,9 +34,8 @@ gulp.task('replace', function() {
 });
 
 gulp.task('copy', function() {
-    gulp.src('./bower_components/webcomponentsjs/webcomponents-lite.min.js')
-      .pipe(gulp.dest('./build'));
-
-    gulp.src('./assets/**/*')
-      .pipe(gulp.dest('./build/assets'));
+    gulp.src('./bower_components/webcomponentsjs/webcomponents-lite.min.js').pipe(gulp.dest('./build'));
+    gulp.src('./service-worker.js').pipe(gulp.dest('./build'));
+    gulp.src('./sw-precache-config.js').pipe(gulp.dest('./build'));
+    gulp.src('./assets/**/*').pipe(gulp.dest('./build/assets'));
 });
