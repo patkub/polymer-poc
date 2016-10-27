55a6b14b14796f072bf3c7f8782a09fadaa7f44d
Patrick Kubiak <patrick@antuple.net>
2016-10-27 13:59:49 -0400

    gulp build with vulcanize

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 
-gulp.task('default', function () {
-    return gulp.src('src/components.html')
-        .pipe(vulcanize({
-            abspath: '',
-            excludes: [],
-            stripExcludes: false,
-            inlineScripts: true,
-            inlineCss: true,
-            stripComments: true
-        }))
-        .pipe(gulp.dest('build/critical.html'));
+gulp.task('default', ['vulcanize', 'replace', 'copy']);
+
+gulp.task('vulcanize', function () {
+    gulp.src('src/components.html')
+      .pipe(vulcanize({
+          stripComments: true,
+          inlineScripts: true,
+          inlineCss: true
+      }))
+      .pipe(rename('critical.html'))
+      .pipe(gulp.dest('./build'));
+});
+
+gulp.task('replace', function() {
+    gulp.src('index.html')
+      .pipe(replace('src/components.html', 'critical.html'))
+      .pipe(replace('/bower_components/webcomponentsjs/webcomponents-lite.min.js', 'webcomponents-lite.min.js'))
+      .pipe(gulp.dest('./build'));
+});
+
+gulp.task('copy', function() {
+    gulp.src('./bower_components/webcomponentsjs/webcomponents-lite.min.js')
+      .pipe(gulp.dest('./build'));
+
+    gulp.src('./assets/**/*')
+      .pipe(gulp.dest('./build/assets'));
 });
