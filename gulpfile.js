37984367ddbf08e451211e77b5186de0177d221f
Patrick Kubiak <patrick@antuple.net>
2016-10-26 21:59:01 -0400

    display cards for projects and websites

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+
+gulp.task('default', function () {
+    return gulp.src('index.html')
+        .pipe(vulcanize({
+            abspath: '',
+            excludes: [],
+            stripExcludes: false,
+            inlineScripts: true,
+            inlineCss: true,
+            stripComments: true
+        }))
+        .pipe(gulp.dest('dest'));
+});
